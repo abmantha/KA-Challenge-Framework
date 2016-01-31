7bb53d23a7e75dbb845ee24f3801b96c8425c66c
abmantha <abhishek.mantha@gmail.com>
2016-01-31 09:33:57 -0800

    Initializing stack

---
+++ ka-src-2/client/index/app.js
@@ -0,0 +1,8 @@
+var React = require('react');
+var TextEditor = require('./components/TextEditor.jsx');
+var KATest = require('./testing-utils/ka-tester');
+
+ReactDOM.render(
+  <TextEditor />,
+  document.getElementById('render-target')
+);
