39e9bfb0214b49ec45c774ea341e697cafc354cd
abmantha <abhishek.mantha@gmail.com>
2016-02-05 11:49:54 -0800

    finishing ui

--- src/client/index/ChallengeFramework.jsx
+++ src/client/index/ChallengeFramework.jsx
@@ -84,38 +84,41 @@ var ChallengeFramework = React.createClass({
 
   render: function() {
     return (
-      <div className="container-fluid ChallengeFramework">
-        <div className="page-header">
+      <div className="ChallengeFramework">
+        <nav className="navbar khan-navbar">
+          <img src="img/leaf-green.svg" className="khan-img img-responsive" alt="Khan Academy"/> 
           <h1> Khan Academy CS Challenge Framework </h1>
-        </div>
-        <div className="row challenge-editor">
-          <div className="col-md-4">
-            WhiteList Check:  <input className="form-control" type="text" value={this.state.whiteListTest} onChange={this.updateWhiteListTest} />
+        </nav>
+        <div className="container-fluid">
+          <div className="row challenge-editor">
+            <div className="col-md-4">
+              WhiteList Check:  <input className="form-control" type="text" value={this.state.whiteListTest} onChange={this.updateWhiteListTest} />
+            </div>
+            <div className="col-md-4">
+              BlackList Check: <input className="form-control" type="text" value={this.state.blackListTest} onChange={this.updateBlackListTest} />
+            </div>
+            <div className="col-md-4">
+              Rough Structure Check: <input type="text" className="form-control" value={this.state.roughStructureTest} onChange={this.updateRoughStructureTest} />
+            </div>
           </div>
-          <div className="col-md-4">
-            BlackList Check: <input className="form-control" type="text" value={this.state.blackListTest} onChange={this.updateBlackListTest} />
-          </div>
-          <div className="col-md-4">
-            Rough Structure Check: <input type="text" className="form-control" value={this.state.roughStructureTest} onChange={this.updateRoughStructureTest} />
-          </div>
-        </div>
 
-        <div className="row coding-editor">
-          <div className="col-md-6 col-md-offset-3">
-            <textarea className="form-control" rows="10" type="text" value={this.state.code} onChange={this.runTests}/>
-            <button className="btn btn-success" onClick={this.reset} type="button">Reset</button>
+          <div className="row coding-editor">
+            <div className="col-md-6 col-md-offset-3">
+              <textarea className="form-control" rows="10" type="text" value={this.state.code} onChange={this.runTests}/>
+              <button className="btn btn-success" onClick={this.reset} type="button">Reset</button>
+            </div>
           </div>
-        </div>
 
-        <div className="row challenge-results">
-          <div className="col-md-3 col-md-offset-1">
-            <h3> WhiteList Result: {this.state.whiteListResult} </h3>
-          </div>
-          <div className="col-md-3">
-            <h3> BlackList Result: {this.state.blackListResult} </h3>
-          </div>
-          <div className="col-md-3">
-            <h3> RoughStructure Result: {this.state.roughStructureResult} </h3> 
+          <div className="row challenge-results">
+            <div className="col-md-3 col-md-offset-1">
+              <h3> WhiteList Result: {this.state.whiteListResult} </h3>
+            </div>
+            <div className="col-md-3">
+              <h3> BlackList Result: {this.state.blackListResult} </h3>
+            </div>
+            <div className="col-md-3">
+              <h3> RoughStructure Result: {this.state.roughStructureResult} </h3> 
+            </div>
           </div>
         </div>
       </div>
